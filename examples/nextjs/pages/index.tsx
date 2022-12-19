e1cc0692b79bde2e0fb6e23931d81a4b404112e3
Bryan Tan <bryantanjw01@gmail.com>
2022-12-19 02:39:24 -0800

    Load open markets in nextjs example (#48)

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
-import { Card, Link as MuiLink } from '@mui/material'
+import { Card, Link as MuiLink, Skeleton } from '@mui/material'
 import Head from 'next/head'
 import Link from 'next/link'
+import { getMarketAccountsByStatus, MarketStatus } from '@monaco-protocol/client'
+import { PublicKey } from "@solana/web3.js"
 import { Page } from '../components/Page'
+import { useProgram } from '../context/ProgramProvider'
 
 const Home: NextPage = () => {
-  const marketAccount = "4R6w8Q52jjnXdJB26K9ML9zUSzthscP3EWhvNZ2WyiAS"
+  const program = useProgram();
+  const [marketAccount, setMarketAccount] = useState<PublicKey>();
+
+  useEffect(() => {
+    const getOpenMarketAccount = async () => {
+      const res = await getMarketAccountsByStatus(program, MarketStatus.Open);
+
+      // Only get an open market with a non-zero marketOutcomesCount
+      res.data.markets.forEach(market => {
+        if (market.account.marketOutcomesCount !== 0) {
+          setMarketAccount(market.publicKey);
+          return;
+        }
+      });
+    }
+    getOpenMarketAccount().catch(console.error);
+  }, []);
+
   return (
     <Page
       title="NextJS Monaco Protocol"
@@ -16,27 +37,37 @@ const Home: NextPage = () => {
         <meta name="description" content="Example usage of the protocol" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Card sx={{
         padding: '1rem',
       }}>
-        <ul>
-          <li>
-            <Link href={`/markets/${marketAccount}/my-bets`}>
-              <MuiLink>
-                My bets
-              </MuiLink>
-            </Link>
-          </li>
-          <li>
-            <Link href={`/markets/${marketAccount}/place-bet`}>
+        {marketAccount === undefined 
+          ? (
+            <>
+            <Skeleton variant="text" sx={{ fontSize: '1rem', width: "30%" }} />
+            <Skeleton variant="text" sx={{ fontSize: '1rem', width: "30%" }} />
+            </>
+          )
+          :
+          <ul>
+            <li>
+              <Link href={`/markets/${marketAccount.toBase58()}/my-bets`}>
                 <MuiLink>
-                  Place bet
+                  My bets
                 </MuiLink>
               </Link>
-          </li>
-        </ul>
-        </Card>
-      </Page>
+            </li>
+            <li>
+              <Link href={`/markets/${marketAccount.toBase58()}/place-bet`}>
+                  <MuiLink>
+                    Place bet
+                  </MuiLink>
+                </Link>
+            </li>
+          </ul>
+        }
+      </Card>
+    </Page>
   )
 }
 
