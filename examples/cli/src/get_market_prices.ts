25ef8da8b954fc330e78892f65f4678aa0d26e96
Drew Spencer <druspencer@gmail.com>
2023-08-01 17:09:17 +0100

    chore: parse empty queues and price ladders

--- examples/cli/src/get_market_prices.ts
+++ examples/cli/src/get_market_prices.ts
@@ -1,12 +1,17 @@
 import { getMarketPrices } from "@monaco-protocol/client";
 import { PublicKey } from "@solana/web3.js";
 import { getProgram, getProcessArgs, logResponse } from "./utils";
-import { parseResponseData } from "./parsers/parsers";
+import { parseEmptyQueueItemsFromMarketPrices, parseResponseData } from "./parsers/parsers";
 
 async function getMarketPricesByPk(marketPk: PublicKey) {
   const program = await getProgram();
   const response = await getMarketPrices(program, marketPk);
   response.data = parseResponseData(response.data)
+  response.data.marketPrices = parseEmptyQueueItemsFromMarketPrices(response.data.marketPrices)
+  
+  // remove price ladder from response as they contain 317 items, comment out line below to see full ladder
+  response.data.marketOutcomeAccounts.forEach((outcome) => { outcome.account.priceLadder = [] })
+  
   logResponse(response);
 }
 
