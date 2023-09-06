eae28f29826766fb848864e9a101fb0867153c90
Drew Spencer <druspencer@gmail.com>
2023-09-06 12:16:40 +0100

    feat: add product to example order placement

--- examples/cli/src/orders/place_for_order.ts
+++ examples/cli/src/orders/place_for_order.ts
@@ -8,13 +8,15 @@ async function placeOrder(marketPk: PublicKey) {
   const forOutcome = true;
   const price = 2;
   const stake = 1;
+  const sdkExampleProductPk = new PublicKey('bwCvZn6Hs4v51tvwFdAtAyJXzLddjgUMnQn2SehXmhF')
   const response = await createOrderUiStake(
     program,
     marketPk,
     marketOutcomeIndex,
     forOutcome,
     price,
-    stake
+    stake,
+    sdkExampleProductPk
   );
   logResponse(response);
 }
