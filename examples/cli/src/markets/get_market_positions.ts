1f5eeba670f80ebf1e166d0f5b8e6efc71277961
Dru (he/him) <drew@betdex.com>
2023-09-21 11:59:58 +0100

    feat: get market positions example (#110)

---
+++ examples/cli/src/markets/get_market_positions.ts
@@ -0,0 +1,16 @@
+import { MarketPositions } from "@monaco-protocol/client";
+import { PublicKey } from "@solana/web3.js";
+import { getProgram, getProcessArgs, logResponse } from "../utils/utils";
+import { AnchorProvider } from "@coral-xyz/anchor";
+import { parseResponseData } from "../parsers/parsers";
+
+async function getMarketPositionForProvider(marketPk: PublicKey) {
+  const program = await getProgram();
+  const provider = program.provider as AnchorProvider;
+  const response = await MarketPositions.marketPositionQuery(program).filterByMarket(marketPk).fetch()
+  response.data = parseResponseData(response.data)
+  logResponse(response);
+}
+
+const args = getProcessArgs(["marketPk"], "npm run getMarketPositionsForMarket");
+getMarketPositionForProvider(new PublicKey(args.marketPk));
