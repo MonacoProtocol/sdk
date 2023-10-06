37224385d8c3dcc3ad6220206709b0a09c0926ce
Drew Spencer <druspencer@gmail.com>
2023-10-06 11:20:17 -0700

    docs: price ladder account examples

---
+++ examples/cli-admin/src/price_ladder/get_ladder.ts
@@ -0,0 +1,13 @@
+import { findPriceLadderPda } from "@monaco-protocol/admin-client";
+import { getProgram, getProcessArgs } from "../utils";
+
+async function getLadder(priceLadderName: string) {
+  const program = await getProgram();
+  const pda = findPriceLadderPda(program, priceLadderName);
+  const response = await program.account.priceLadder.fetch(pda.data.pda);
+  console.log(JSON.stringify(response, null, 2));
+}
+
+const args = getProcessArgs(["priceLadderName"], "npm run getLadder");
+getLadder(args.priceLadderName);
+
