77550df95972e2f1d64f29b59420efb2d7960477
Dru (he/him) <drew@betdex.com>
2023-12-01 11:07:42 +0000

    docs: example get market type by pk (#119)

--- examples/cli-admin/src/market_types/get_market_type_by_name.ts
+++ examples/cli-admin/src/market_types/get_market_type_by_name.ts
@@ -2,12 +2,12 @@
 import { findMarketTypePda } from "@monaco-protocol/admin-client";
 import { getProgram, getProcessArgs } from "../utils";
 
-async function getMarketTypes(marketTypeName: string) {
+async function getMarketType(marketTypeName: string) {
   const program = await getProgram();
   const pda = findMarketTypePda(program, marketTypeName);
   const response = await program.account.marketType.fetch(pda.data.pda);
   console.log(JSON.stringify(response, null, 3));
 }
 
-const args = getProcessArgs(["marketTypeName"], "npm run getMarketType");
-getMarketTypes(args.marketTypeName);
+const args = getProcessArgs(["marketTypeName"], "npm run getMarketTypeByName");
+getMarketType(args.marketTypeName);
