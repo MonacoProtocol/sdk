18be95fed8c24ea313303766d5df7a47e9f00004
Drew Spencer <druspencer@gmail.com>
2024-07-17 13:09:13 +0100

    docs: update cli example for 14.x

--- examples/cli/src/markets/get_market_matching_pools.ts
+++ examples/cli/src/markets/get_market_matching_pools.ts
@@ -2,7 +2,6 @@ import { getAllMarketMatchingPools } from "@monaco-protocol/client";
 import { PublicKey } from "@solana/web3.js";
 import { getProgram, getProcessArgs, logResponse } from "../utils/utils";
 import {
-  parseEmptyQueueItemsFromMatchingPoolAccounts,
   parseResponseData
 } from "../parsers/parsers";
 
@@ -10,7 +9,6 @@ async function marketMatchingPools(marketPk: PublicKey) {
   const program = await getProgram();
   const response = await getAllMarketMatchingPools(program, marketPk);
   response.data = parseResponseData(response.data);
-  response.data = parseEmptyQueueItemsFromMatchingPoolAccounts(response.data);
   logResponse(response);
 }
 
--- examples/cli/src/markets/get_market_summary.ts
+++ examples/cli/src/markets/get_market_summary.ts
@@ -1,100 +1,117 @@
 import {
-  findEscrowPda,
-  getMarketPrices,
+  findMarketLiquiditiesPda,
+  getMarket,
+  getMarketLiquidities,
+  getMarketOutcomesByMarket,
   getMintInfo,
-  MarketOutcomeAccount,
-  MarketPrice
+  MarketLiquidities,
 } from "@monaco-protocol/client";
 import { PublicKey } from "@solana/web3.js";
-import { mapPricesToOutcomesAndForAgainst } from "../mappers/market_price_mapper";
-import { mapOrdersToOutcomesAndForAgainst } from "../mappers/order_mapper";
 import { getProgram, getProcessArgs, logJson } from "../utils/utils";
 import { AnchorProvider } from "@coral-xyz/anchor";
-import { parseResponseData } from "../parsers/parsers";
+import { integerToUiValue } from "../parsers/parsers";
 
 async function getMarketSummary(marketPk: PublicKey) {
   const program = await getProgram();
   const provider = program.provider as AnchorProvider;
-  const escrowPda = await findEscrowPda(program, marketPk);
-  const [marketPrices, escrow] = await Promise.all([
-    getMarketPrices(program, marketPk),
-    provider.connection.getParsedAccountInfo(escrowPda.data.pda)
+  const liquiditiesPda = await findMarketLiquiditiesPda(program, marketPk);
+  const [marketLiquidity, market, marketOutcomes] = await Promise.all([
+    getMarketLiquidities(program, liquiditiesPda.data.pda),
+    getMarket(program, marketPk),
+    getMarketOutcomesByMarket(program, marketPk),
   ]);
   const mintDetails = await getMintInfo(
     program,
-    marketPrices.data.market.mintAccount
-  );
-  const parsedMarketPrices = parseResponseData(
-    marketPrices.data,
-    mintDetails.data.decimals
-  );
-  const marketOutcomeAccounts = parsedMarketPrices.marketOutcomeAccounts.map(
-    (marketOutcomeAccount) => {
-      return marketOutcomeAccount.account;
-    }
-  );
-  const matchedTotal = matchedTotalFromParsedOutcomeAccounts(
-    marketOutcomeAccounts
-  );
-  const liquidityTotal = liquidityTotalFromParsedMarketPrices(
-    parsedMarketPrices.marketPrices
-  );
-  const marketOutcomesSummary = parsedMarketPrices.marketOutcomeAccounts.map(
-    (outcome) => {
-      return {
-        outcome: outcome.account.title,
-        latestMatchedPrice: outcome.account.latestMatchedPrice,
-        matchedTotal: outcome.account.matchedTotal
-      };
-    }
+    market.data.account.mintAccount
   );
 
-  const outcomeTitles = marketOutcomeAccounts.map((outcome) => {
-    return outcome.title;
-  });
-  const pendingOrders = parsedMarketPrices.pendingOrders.map((order) => {
-    return order.account;
+  const outcomeTitles = marketOutcomes.data.marketOutcomeAccounts.map((outcome) => {
+    return outcome.account.title;
   });
 
-  const pendingOrderSummary = mapOrdersToOutcomesAndForAgainst(
-    outcomeTitles,
-    pendingOrders
-  );
-  const marketPriceSummary = mapPricesToOutcomesAndForAgainst(
-    outcomeTitles,
-    parsedMarketPrices.marketPrices
-  );
+  const prices = marketPrices(marketLiquidity.data.account, outcomeTitles, mintDetails.data.decimals);
+  const priceMatrix = generatePriceMatrix(prices);
 
   logJson({
-    pendingOrderSummary: pendingOrderSummary,
-    marketPriceSummary: marketPriceSummary,
-    marketOutcomesSummary: marketOutcomesSummary,
-    marketTitle: parsedMarketPrices.market.title,
-    marketLock: new Date(parsedMarketPrices.market.marketLockTimestamp * 1000),
-    liquidityTotal: liquidityTotal,
-    matchedTotal: matchedTotal,
-    totalUnmatchedOrders: parsedMarketPrices.pendingOrders.length,
-    totalInEscrow:
-      escrow.value.data["parsed"]["info"]["tokenAmount"]["uiAmount"]
+    marketTitle: market.data.account.title,
+    marketLock: new Date(market.data.account.marketLockTimestamp * 1000).toUTCString(),
+    liquidityTotal: liquidityTotal(marketLiquidity.data.account, mintDetails.data.decimals),
+    matchedTotal: integerToUiValue(marketLiquidity.data.account.stakeMatchedTotal, mintDetails.data.decimals),
   });
+
+  console.table(priceMatrix);
 }
 
-function matchedTotalFromParsedOutcomeAccounts(
-  marketOutcomeAccounts: MarketOutcomeAccount[]
-) {
-  let marketMatchedTotal = 0;
-  marketOutcomeAccounts.map((marketOutcome) => {
-    marketMatchedTotal += marketOutcome.matchedTotal;
+function liquidityTotal(marketLiquidity: MarketLiquidities, mintDecimals: number) {
+  let liquidityTotal = 0;
+  for (const liquidity of marketLiquidity.liquiditiesFor) {
+    liquidityTotal += liquidity.liquidity.toNumber();
+  }
+  for (const liquidity of marketLiquidity.liquiditiesAgainst) {
+    liquidityTotal += liquidity.liquidity.toNumber();
+  }
+  return liquidityTotal / 10 ** mintDecimals;
+};
+
+type MarketPrices = {
+  [outcomeTitle: string]: {
+    for: { price: number; liquidity: number }[];
+    against: { price: number; liquidity: number }[];
+  };
+};
+
+type PriceMatrix = {
+  outcome: string;
+  for3: string;
+  for2: string;
+  for1: string;
+  against1: string;
+  against2: string;
+  against3:string;
+};
+
+function marketPrices(marketLiquidity: MarketLiquidities, outcomeTitles: string[], mintDecimals: number): MarketPrices {
+  const prices = {};
+
+  outcomeTitles.forEach((title, index) => {
+    const liquiditiesFor = marketLiquidity.liquiditiesFor.filter((liquidity) => liquidity.outcome === index) ?? [];
+    const liquiditiesAgainst = marketLiquidity.liquiditiesAgainst.filter((liquidity) => liquidity.outcome === index) ?? [];
+
+    prices[title] = {
+      for: liquiditiesFor.map((liquidityFor) => ({
+        price: liquidityFor.price,
+        liquidity: integerToUiValue(liquidityFor.liquidity, mintDecimals),
+      })),
+      against: liquiditiesAgainst.map((liquidityAgainst) => ({
+        price: liquidityAgainst.price,
+        liquidity: integerToUiValue(liquidityAgainst.liquidity, mintDecimals),
+      })),
+    };
   });
-  return marketMatchedTotal;
+
+  return prices;
 }
 
-function liquidityTotalFromParsedMarketPrices(marketPrices: MarketPrice[]) {
-  let marketLiquidity = 0;
-  marketPrices.map((marketPrice) => {
-    marketLiquidity += marketPrice.matchingPool.liquidityAmount;
+function generatePriceMatrix(marketPrices: MarketPrices): PriceMatrix[] {
+  return Object.entries(marketPrices).map(([outcomeTitle, prices]) => {
+    const sortedForPrices = prices.for.sort((a, b) => a.price - b.price);
+    const sortedAgainstPrices = prices.against.sort((a, b) => a.price - b.price);
+
+    const getPrice = (arr: { price: number; liquidity: number }[], index: number) => {
+      const price = arr[index] ?? { price: 0, liquidity: 0 };
+      return price.price ? `${price.price} ($${price.liquidity})` : '-';
+    };
+
+    return {
+      outcome: outcomeTitle,
+      for3: getPrice(sortedAgainstPrices, 2),
+      for2: getPrice(sortedAgainstPrices, 1),
+      for1: getPrice(sortedAgainstPrices, 0),
+      against1: getPrice(sortedForPrices, 0),
+      against2: getPrice(sortedForPrices, 1),
+      against3: getPrice(sortedForPrices, 2),
+    };
   });
-  return marketLiquidity;
 }
 
 const args = getProcessArgs(["marketPk"], "npm run getMarketSummary");
