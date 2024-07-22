044801adda8b8e4370fc16a9fa4d78f7bbac8720
Drew Spencer <druspencer@gmail.com>
2024-07-22 11:26:37 +0100

    docs: cli example for market position for wallet

---
+++ examples/cli/src/markets/get_market_positions_for_wallet.ts
@@ -0,0 +1,40 @@
+import { MarketPositions, getMarketOutcomeTitlesByMarket } from "@monaco-protocol/client";
+import { PublicKey } from "@solana/web3.js";
+import { getProgram, getProcessArgs, logResponse } from "../utils/utils";
+import { AnchorProvider } from "@coral-xyz/anchor";
+import { parseResponseData } from "../parsers/parsers";
+
+async function getMarketPositionForProvider(walletPk: PublicKey) {
+  const program = await getProgram();
+  const provider = program.provider as AnchorProvider;
+  const response = await MarketPositions.marketPositionQuery(program)
+    .filterByPurchaser(walletPk)
+    .filterByPaid(false)
+    .fetch();
+  response.data = parseResponseData(response.data, 6);
+  const positions = {};
+  for (const position of response.data.marketPositionAccounts) {
+    const outcomeTitles = await getMarketOutcomeTitlesByMarket(program, position.account.market);
+    const matchedPosition = position.account.marketOutcomeSums.map((exposure, index) => {
+      return {
+        outcome: outcomeTitles.data.marketOutcomeTitles[index],
+        exposure: exposure,
+        unmatched: position.account.unmatchedExposures[index]
+      }
+    });
+
+    positions[position.account.market.toBase58()] = {
+      position: matchedPosition,
+    }
+  }
+  for (const market in positions) {
+    console.log(`Market: ${market}`);
+    console.table(positions[market].position);
+  }
+}
+
+const args = getProcessArgs(
+  ["walletPk"],
+  "npm run getMarketPositionsForWallet"
+);
+getMarketPositionForProvider(new PublicKey(args.walletPk));
